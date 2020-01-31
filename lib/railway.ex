69997139715ba3c277db392d4afb401efc00fdb5
DJ Bigelow <djbigelow0@gmail.com>
2020-01-31 10:00:40 -0700

    Railway lesson done

--- lib/railway.ex
+++ lib/railway.ex
@@ -16,7 +16,57 @@ defmodule CodeFlow.Railway do
   Works well when the functions are designed to pass the output of one
   step as the input of the next function.
   """
-  def award_points(%User{} = _user, _inc_point_value) do
+  def award_points(%User{} = user, inc_point_value) do
+    user
+    |> validate_is_active
+    |> validate_at_least_age(16)
+    |> check_name_blacklist
+    |> increment_points(inc_point_value)
+  end
 
+
+  def validate_is_active(user) do
+    case user do
+      %{active: true} -> {:ok, user}
+      _other -> {:error, "Not an active User"}
+    end
+  end
+
+
+  def validate_at_least_age({:ok, %User{age: age} = user }, age_cutoff) when age >= age_cutoff do
+    {:ok, user}
+  end
+
+  def validate_at_least_age({:ok, _user}, _age_cutoff) do
+    {:error, "User age is below the cutoff"}
+  end
+
+  def validate_at_least_age(other, _) do
+    other
+  end
+
+
+
+
+  def check_name_blacklist({:ok, %User{} = user}) do
+    blacklist = ["Tom", "Tim", "Tammy"]
+    if !Enum.member?(blacklist, user.name) do
+      {:ok, user}
+    else
+      {:error, "User #{inspect(user.name)} is blacklisted"}
+    end
+  end
+
+  def check_name_blacklist(other) do
+    other
+  end
+
+
+  def increment_points({:ok, %User{points: user_points} = user}, points) do
+    {:ok, %{user | points: user_points + points}}
+  end
+
+  def increment_points(other, _) do
+    other
   end
 end
