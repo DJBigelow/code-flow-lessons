a4736611e04cc74e5642b8af92725de5a2edd5c7
DJ Bigelow <djbigelow0@gmail.com>
2020-01-31 11:05:09 -0700

    Finished recursion lession

--- lib/recursion.ex
+++ lib/recursion.ex
@@ -10,31 +10,59 @@ defmodule CodeFlow.Recursion do
   @moduledoc """
   Fix or complete the code to make the tests pass.
   """
-  # alias CodeFlow.Fake.Customers
-  # alias CodeFlow.Schemas.OrderItem
+  alias CodeFlow.Fake.Customers
+  alias CodeFlow.Schemas.OrderItem
+  alias CodeFlow.Schemas.Customer
+  alias CodeFlow.Schemas.Item
 
   @doc """
   Sum a list of OrderItems to compute the order total.
   """
-  def order_total(_order_items) do
-
+  def order_total(order_items) do
+    do_order_total(order_items, 0.0)
   end
+
+  defp do_order_total([%OrderItem{} = order | tail], acc) do
+    do_order_total(tail, acc + order.item.price * order.quantity)
+  end
+
+  defp do_order_total([], acc), do: acc
 
   @doc """
   Count the number of active customers. Note: Normally this would be done with a
   query to an SQL database. This is just to practice conditionally incrementing
   a counter and looping using recursion.
   """
-  def count_active(_customers) do
-
+  def count_active(customers) do
+    do_count_active(customers, 0)
   end
+
+  defp do_count_active([%Customer{active: active} | tail], acc) do
+    if active == true do
+       do_count_active(tail, acc + 1)
+    else
+      do_count_active(tail, acc)
+  end
+  end
+
+  defp do_count_active([], acc), do: acc
 
   @doc """
   Create the desired number of customers. Provide the number of customers to
   create. Something like this could be used in a testing setup.
   """
-  def create_customers(_number) do
+  def create_customers(number) do
+    do_create_customers(number, 0)
+  end
 
+  defp do_create_customers(number, total) when total < number do
+    # for simplicity, not handling a failed create
+    {:ok, _customer} = Customers.create(%{name: "Customer #{number}"})
+    do_create_customers(number, total + 1)
+  end
+
+  defp do_create_customers(_number, total) do
+    "Created #{total} customers!"
   end
 
   @doc """
@@ -43,7 +71,13 @@ defmodule CodeFlow.Recursion do
   sequence.
   https://en.wikipedia.org/wiki/Fibonacci_number
   """
-  def fibonacci(_num) do
-
+  def fibonacci(n) do
+    cond  do
+      n == 0 -> 0
+      n == 1 -> 1
+      n > 1 -> fibonacci(n - 1) + fibonacci(n - 2)
+    end
   end
+
+
 end
